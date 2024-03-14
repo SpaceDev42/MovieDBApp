72736e0ceb6a04d663b2c5a00e00785b365120d1
Vladimir Guevara <gvguevara@applaudostudios.com>
2024-03-14 16:21:24 -0600

    Initial Commit

---
+++ MovieDBApp/MovieDBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBAppApp.swift
+//  MovieDBApp
+//
+//  Created by Vladimir Guevara on 14/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
